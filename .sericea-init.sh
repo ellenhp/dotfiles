0b7bbd010c95d40b42386736f28cf5f07ec3bb3d
Ellen Poe <ellen.h.poe@gmail.com>
2024-03-22 08:44:56 -0700

    I'm never going to use sericea again probably

--- .sericea-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-toolbox create
-rpm-ostree install --apply-live zsh tmux vim htop util-linux-user wofi NetworkManager-tui
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-chsh --shell /bin/zsh
-swaymsg reload
